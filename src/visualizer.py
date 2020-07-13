e289b92d50d2bf1dc3356d1db00152082acbd389
René Kost <git@rkost.org>
2020-07-13 23:24:19 +0200

    Better axis labels, add requirements, tuning

--- src/visualizer.py
+++ src/visualizer.py
@@ -7,6 +7,7 @@ import altair as alt
 def plot_ribbon_plot(data, interval_string, output_file):
     data.index = data.index.floor(interval_string)
 
+    # Generate
     interval_data = pd.concat([
         data.groupby(data.index).quantile(0.05).rename(columns={"Upload Speed (MBit/s)": "q5"}),
         data.groupby(data.index).quantile(0.10).rename(columns={"Upload Speed (MBit/s)": "q10"}),
@@ -18,25 +19,25 @@ def plot_ribbon_plot(data, interval_string, output_file):
     ], axis=1)
 
     # Disclaimer: This was written by @rkost while he did not know anything about altair.
-    # Suggestions welcome!
+    # It basically is a giant hack (I guess). Suggestions welcome!
     alt.data_transformers.disable_max_rows()
     line = alt.Chart(interval_data.reset_index()).mark_line().encode(
-        x='index',
-        y='q50:Q'
+        x=alt.X('index', axis=alt.Axis(title='Time (CEST)')),
+        y=alt.Y('q50:Q', axis=alt.Axis(title='MBit/s: Mean'))
     ).properties(width=1000, height=500)
     area50 = alt.Chart(interval_data.reset_index()).mark_area(opacity=0.2).encode(
         x='index',
-        y='q75',
+        y=alt.Y('q75:Q', axis=alt.Axis(title='confidence 50')),
         y2='q25'
     )
     area80 = alt.Chart(interval_data.reset_index()).mark_area(opacity=0.2).encode(
         x='index',
-        y='q90',
+        y=alt.Y('q90:Q', axis=alt.Axis(title='confidence 80')),
         y2='q10'
     )
     area90 = alt.Chart(interval_data.reset_index()).mark_area(opacity=0.2).encode(
         x='index',
-        y='q95',
+        y=alt.Y('q95:Q', axis=alt.Axis(title='confidence 90')),
         y2='q05'
     )
     scatter = alt.Chart(data.reset_index()).mark_point(opacity=0.05, filled=True).encode(
@@ -45,6 +46,7 @@ def plot_ribbon_plot(data, interval_string, output_file):
     )
 
     plot = (line + area50 + area80 + area90 + scatter)
+    plot.save(output_file, scale_factor=2.0)
     plot.show()
 
 
@@ -53,5 +55,4 @@ def plot_box_plot(data, interval_string, output_file):
     fig = plt.figure(figsize=(24, 9))
     sns.set(style="ticks")
     sns.boxplot(data.index, data["Upload Speed (MBit/s)"])
-    plt.show()
-    # plt.savefig(output_file)
+    plt.savefig(output_file)
